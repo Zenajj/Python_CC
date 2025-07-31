0a0aa950537beaf61299ff331fb98afd289972b5
Zenajj <zenaj.game@gmail.com>
2025-07-31 15:11:26 +0200

    Add chapter 8 exercise 8-2 to Ch8_1.py

--- Ch8_1.py
+++ Ch8_1.py
@@ -1,6 +1,12 @@
+def favorite_book(title):
+    '''Function tells everyone about my favourite book.'''
+    print(f"One of my favorite books is {title.title()}.")
+
 def greet_user(username):
     """Display a simple greeting."""
     print(f"Hello {username.title()}!")
 
+book_title = input("What's your favourite book? ")
+favorite_book(book_title)
 user_name = input("Your username: ")
 greet_user(user_name)
